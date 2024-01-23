779057b2e02f5fde00ef8868a3f293b4eb791bb5
ZHAO Changfeng <changfeng.zhao@hotmail.com>
2024-01-23 14:50:02 -0500

    rajouter un Get/hello

--- ListeRepertoire_Backend/server.js
+++ ListeRepertoire_Backend/server.js
@@ -5,6 +5,8 @@ const app = express()
 app.use(express.json())
 
 //REST_API
+app.get('/api/hello', (requete, reponse) => { reponse.send("Hello World!") })
+
 
 app.listen(8000, () => console.log('Ecoute le port 8000'))
 
