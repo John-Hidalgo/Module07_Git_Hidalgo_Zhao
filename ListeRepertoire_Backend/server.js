54a0ebb9941807f971aed1258ad4710161b03586
John Hidalgo <2297119@csfoy.ca>
2024-01-24 14:29:27 -0500

    installation du mongoDB et ajoute du handlers

--- ListeRepertoire_Backend/server.js
+++ ListeRepertoire_Backend/server.js
@@ -1,8 +1,45 @@
 import express from "express"
+import { MongoClient } from 'mongodb';
 
 
 const app = express()
 app.use(express.json())
+
+app.get('/api/pieces/:id ', GererObtiensUnePiece);
+app.post('/api/pieces/ajouter ', GererAjouterUnePiece);
+app.put('/api/pieces/:id/modifier ', GererModifierUnePiece);
+
+async function GererObtiensUnePiece(req,rep)
+{
+
+}
+async function GererAjouterUnePiece(req, rep)
+{
+
+}
+async function GererModifierUnePiece(req, rep)
+{
+
+}
+
+
+
+
+async function UtiliserBD(operations, reponse)
+{
+    try
+    {
+        const client = await MongoClient.connect('mongodb://0.0.0.0:27017');
+        const BD = client.db('repertoire');
+        await operations(BD);
+        client.close();
+    }
+    catch (e)
+    {
+        response.status(500).json({ message: 'Erreur de connexion à la base de donnée:', e });
+    }
+    
+}
 
 //REST_API
 app.get('/api/hello', (requete, reponse) => { reponse.send("Hello World!") })
