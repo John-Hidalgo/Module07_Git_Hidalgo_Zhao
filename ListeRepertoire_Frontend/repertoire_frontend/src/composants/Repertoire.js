73c918eb83e93fde39d2847d39106487de1b1d89
John Hidalgo <2297119@csfoy.ca>
2024-01-24 21:16:18 -0500

    ajoute du composant pour importer les pieces

---
+++ ListeRepertoire_Frontend/repertoire_frontend/src/composants/Repertoire.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+const Repertoire = () =>
+{
+    const { pieces, setPieces } = useState([]);
+    useEffect(() =>{
+        const obtiensPieces = async () =>
+        {
+            try
+            {
+                const reponse = await fetch('/api/pieces');
+                const data = await reponse.json();
+                setPieces(data);
+            }
+            catch (erreur)
+            {
+                console.error(erreur);
+            }
+        };
+        obtiensPieces();
+    }, []);
+    return pieces;
+}
+
+export default Repertoire;
--- ListeRepertoire_Frontend/repertoire_frontend/src/pages/pageRepertoireClients.js
+++ ListeRepertoire_Frontend/repertoire_frontend/src/pages/pageRepertoireClients.js
@@ -1,1 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Repertoire from '../composants/Repertoire';
+
+const PageRepertoireClients = () =>
+{
+    const repertoire = Repertoire();
+    return (
+        <>
+            <h1>Liste du repertoire</h1>
+            {
+                repertoire.map(p => (p.titre
+                ))
+            }
+        </>
+    )
+}
+
+export default PageRepertoireClients;
