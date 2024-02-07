94e7690c4907edc57adc7df4c5e7f233bf442dbf
John Hidalgo <2297119@csfoy.ca>
2024-02-07 05:46:14 -0500

    page pour voir toute les pieces

---
+++ ListeRepertoire_Frontend/src/pages/admin/pagePiecesAdmin.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+import { Navigate, useNavigate } from 'react-router-dom';
+import ObtiensPieces from '../../composants/ObtiensPieces.js';
+import EffacerPiece from '../../composants/EffacerPiece.js';
+import { Button, ListGroup } from 'react-bootstrap';
+const PagePiecesAdmin = () => 
+{
+    const history = useNavigate();
+    const [pieces, setPieces] = useState([]);
+    useEffect(() => 
+    {
+        const fetchInitiale = async () => {
+        const data = await ObtiensPieces();
+        setPieces(data);
+        };
+        fetchInitiale();
+    }, []);
+    const GererModifierPiece = (_id) =>
+    {
+        history(`/modifier-piece/${_id}`);
+    }
+
+    const GererEffacerPiece = async (_id) =>
+    {
+        EffacerPiece(_id,setPieces,ObtiensPieces)
+    }
+    return (
+    <div>
+      <h2>Liste du repertoire</h2>
+      <ListGroup>
+        {pieces.map((p) => (
+          <ListGroup.Item key={p._id}>
+            {p.titre} {p.artiste} {p.categorie}
+            <Button onClick={() => GererModifierPiece(p._id)}> Modifier </Button>
+            <Button onClick={() => GererEffacerPiece(p._id)}> Effacer </Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+    )
+}
+
+export default PagePiecesAdmin;
