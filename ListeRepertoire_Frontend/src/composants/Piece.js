9f345b99fc564e141397527b50008cfff4c500da
John Hidalgo <2297119@csfoy.ca>
2024-02-09 12:10:15 -0500

    ajustament du modifer pour afficher les legacy repertoire

--- ListeRepertoire_Frontend/src/composants/Piece.js
+++ ListeRepertoire_Frontend/src/composants/Piece.js
@@ -1,23 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-const Piece = ({ id }) => 
-{
-    const [piece, setPiece] = useState()
-    useEffect(() => {
-        const obtiensPiece = async () => {
-            try {
-                //console.log("calling fetch");
-                const reponse = await fetch(`/api/pieces/${id}`)
-                const data = await reponse.json()
-                //console.log(data);
-                setPiece(data)
-            }
-            catch (erreur) {
-                console.error(erreur)
-            }
-        }
-        obtiensPiece()
-    }, [id])
-    return piece
-}
-export default Piece
+const Piece = (id) => {
+  const [piece, setPiece] = useState({
+    titre: '',
+    artiste: '',
+    categorie: [''],
+  });
+
+  useEffect(() => {
+    const obtiensPiece = async () => {
+      try {
+        const reponse = await fetch(`/api/pieces/${id}`);
+        const data = await reponse.json();
+        setPiece(data);
+      } catch (erreur) {
+        console.error(erreur);
+      }
+    };
+
+    obtiensPiece();
+  }, [id]);
+
+  return piece;
+};
+
+export default Piece;
--- ListeRepertoire_Frontend/src/pages/admin/pageModiferUnePiece.js
+++ ListeRepertoire_Frontend/src/pages/admin/pageModiferUnePiece.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import ModifierUnePiece from '../../composants/ModifierUnePiece.js'
+import Piece from '../../composants/Piece.js'
 import { InputGroup, FormControl, Button, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 const PageModifierPieceAdmin = () => 
 {
+  const { id } = useParams();
+  const piece = Piece(id);
   const [titre, setTitre] = useState('');
   const [artiste, setArtiste] = useState('');
   const [categories, setCategories] = useState(['']);
@@ -21,7 +24,7 @@ const PageModifierPieceAdmin = () =>
   {
       setCategories([...categories, '']);
   };
-  const { id } = useParams()
+  
 
   const gererModifierUnePiece = async () => 
   {
@@ -29,10 +32,25 @@ const PageModifierPieceAdmin = () =>
   }
 
   return (
-        <Container>
+    <Container>
         <br />
-            Veuillez entrer le nom de l’artiste titre et la catégorie pour modifier la pièce de votre répertoire.
-        <br /><br />
+      Veuillez entrer le nom de l’artiste titre et la catégorie pour modifier la pièce de votre répertoire.
+      <br /><br />
+      <p> Titre : { piece.titre}</p>
+      <p> Artiste : { piece.artiste}</p>  
+      <p>
+        {piece.categorie &&
+          (Array.isArray(piece.categorie)
+            ? piece.categorie.length > 1
+              ? 'Categories'
+              : 'Categorie'
+            : 'Categorie')}
+        :{' '}
+        {piece.categorie &&
+          (Array.isArray(piece.categorie)
+            ? piece.categorie.join(', ')
+            : piece.categorie)}
+      </p>
         <label>
             Titre:
             <InputGroup className="mb-3">
@@ -71,7 +89,7 @@ const PageModifierPieceAdmin = () =>
         <Button onClick={AjouterNouvelleEntree}> Ajoutez une categorie</Button>
         </label>
         <br /><br />
-        <Button onClick={gererModifierUnePiece} disabled={!boutonActif}>Ajouter à votre répertoire </Button>
+        <Button onClick={gererModifierUnePiece} disabled={!boutonActif}>Modifier à votre répertoire </Button>
         </Container>
   )
 }
