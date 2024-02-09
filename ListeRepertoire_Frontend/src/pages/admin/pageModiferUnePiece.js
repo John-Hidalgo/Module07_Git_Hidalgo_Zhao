f0796d593e022a555436d29b7a4192e5f96895ca
John Hidalgo <2297119@csfoy.ca>
2024-02-09 10:09:16 -0500

    regler du bug dans page modifier piece

--- ListeRepertoire_Frontend/src/pages/admin/pageModiferUnePiece.js
+++ ListeRepertoire_Frontend/src/pages/admin/pageModiferUnePiece.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ModifierUnePiece from '../../composants/ModifierUnePiece.js'
+import { InputGroup, FormControl, Button, Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 const PageModifierPieceAdmin = () => 
@@ -28,26 +29,50 @@ const PageModifierPieceAdmin = () =>
   }
 
   return (
-    <div>
-      <label>
-        Titre:
-        <input type="text" value={titre} onChange={(e) => setTitre(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Artiste:
-        <input type="text" value={artiste} onChange={(e) => setArtiste(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Categorie:
-        <input type="text" value={categories} onChange={(e) => setCategories(e.target.value)} />
-      </label>
-      <br />
-      <button onClick={gererModifierUnePiece} disabled={!boutonActif}>
-        Send
-      </button>
-    </div>
+        <Container>
+        <br />
+            Veuillez entrer le nom de l’artiste titre et la catégorie pour modifier la pièce de votre répertoire.
+        <br /><br />
+        <label>
+            Titre:
+            <InputGroup className="mb-3">
+                <FormControl
+                    type="text"
+                    value={titre}
+                    onChange={(e) => setTitre(e.target.value)}
+                />
+            </InputGroup>
+        </label>
+        <br />
+        <label>
+            Artiste:
+            <InputGroup className="mb-3">
+                <FormControl
+                    type="text"
+                    value={artiste}
+                    onChange={(e) => setArtiste(e.target.value)}
+                />
+            </InputGroup>
+        </label>
+        <br />
+        <label>
+            Categories:
+            {categories.map((category, index) => (
+                <div key={index} className="mb-3">
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            value={category}
+                            onChange={(e) => GererAjouterNouvelleCategorie(index, e.target.value)}
+                        />
+                    </InputGroup>
+                </div>
+            ))}
+        <Button onClick={AjouterNouvelleEntree}> Ajoutez une categorie</Button>
+        </label>
+        <br /><br />
+        <Button onClick={gererModifierUnePiece} disabled={!boutonActif}>Ajouter à votre répertoire </Button>
+        </Container>
   )
 }
 
